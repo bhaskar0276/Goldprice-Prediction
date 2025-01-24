6ccdbcbf96b724e0a0bc8b4b55babcd80da75a55
Bhaskar Reddy Thavva <thavva.b@northeastern.edu>
2025-01-23 19:06:56 -0500

    Update logger and exception

--- src/exception.py
+++ src/exception.py
@@ -22,15 +22,3 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
 
-
-
-
-if __name__=="__main__":
-    logging.info("Logging has started")
-
-    try:
-        a=1/1
-        print(a)
-    except Exception as e:
-        logging.info('Dicision by zero')
-        raise CustomException(e,sys)
